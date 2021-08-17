0330a81ce87476963cfdd5ae7da54c347ba9e768
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-08-18 01:19:39 +0300

    Добавляет реализацию задачи 003
    Добавляет реализацию задачи 003

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//---------------------------------------------------------------
+
 const delay = ms => {
   return new Promise(resolve => {
     setTimeout(() => resolve(ms), ms);
@@ -33,3 +35,36 @@ const loggerUsers = updatedUsers => console.table(updatedUsers);
 
 toggleUserState(users, 'Mango').then(loggerUsers);
 toggleUserState(users, 'Lux').then(loggerUsers);
+
+//---------------------------------------------------------------
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  const delay = randomIntegerFromInterval(200, 500);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        resolve([transaction.id, delay]);
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  });
+};
+
+const logSuccess = ([id, time]) => {
+  console.log(`Transaction ${id} processed in ${time}ms`);
+};
+
+const logError = id => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
